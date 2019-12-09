cd40055f44f231b0fc3688a7761193684960537c
GopiKrishna <krish9@GopiKrishnas-MacBook-Pro.local>
2019-12-09 15:53:09 -0500

    Products view

---
+++ src/components/home/Card.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Button, Card } from "reactstrap";
+// import {
+//     withRouter
+//   } from 'react-router-dom'
+// import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+
+export default class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.product._id
+    };
+    this.prod_details = this.prod_details.bind(this);
+  }
+
+  prod_details(e) {
+    //let history = useHistory();
+
+    console.log("inside this function");
+    e.preventDefault();
+
+    var id = this.props.product._id;
+    //history.push(`/product/${id}`)
+    // this.context.router.transitionTo(`/product/${id}`);
+  }
+
+  render() {
+    return (
+      <div className="single_card">
+        <Card style={{ width: "18rem" }}>
+          <CardImg
+            top
+            width="100%"
+            src="/assets/318x180.svg"
+            src="https://cdn.vox-cdn.com/thumbor/T49kxT-ZhzKjyWWWkVYiOIyAOv4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/11249379/jbareham_171101_2099_A_0088_02.jpg"
+          />
+          <CardBody>
+            <CardTitle>{this.props.product.prodname}</CardTitle>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+            <Link to={`/product/${this.state.id}`}>
+              <Button color="primary">View Details</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
---
+++ src/components/home/ProductList.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Cards from "./Card.jsx";
+
+export default class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/api/products")
+      .then(response => {
+        console.log("response" + response.data);
+        this.setState({ products: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  productList() {
+    return this.state.products.map(currentproduct => {
+      return <Cards product={currentproduct} key={currentproduct._id} />;
+    });
+  }
+  render() {
+    return <div>{this.productList()}</div>;
+  }
+}
