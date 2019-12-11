b5f161aee927dca6788a571a1a892dfb67cdae0e
Suryanarayana Malladi <malladi.s@husky.neu.edu>
2019-12-11 15:54:08 -0500

    Fixed product schema

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   prodname: { type: String, required: true },
   username: { type: String, required: true },
-  seller: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: String, required: true },
   date: { type: Date, required: true },
